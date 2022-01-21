167e220a8c51d588d1d414eaca296ee579e23051
Christine Fouque <christinefouque80@gmail.com>
2022-01-21 10:43:02 +0100

    commit A

--- fichier_new.py
+++ fichier_new.py
@@ -2,4 +2,6 @@ je mlodifie mon fichier sur la branche main
 ERREUR Je modifie mon fichier sur la branche main
 je modifie mon fichier mais l'oublie dans mon commit
 
+je fais une modif qui correpond au commit A
+
 
