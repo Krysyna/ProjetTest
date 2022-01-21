7b3ed570cd87b583c60d32f5e4c2ceb0ae27fd03
Christine Fouque <christinefouque80@gmail.com>
2022-01-21 14:36:37 +0100

    Revert "commit 01"
    This reverts commit 193b1dd7a791da76f5765757cae7c1534f21b4f3.

--- fichier_new.py
+++ fichier_new.py
@@ -1,8 +1,11 @@
 je mlodifie mon fichier sur la branche main
 ERREUR Je modifie mon fichier sur la branche main
 je modifie mon fichier mais l'oublie dans mon commit
-----
+
+<<<<<<< HEAD
 je fais une modif qui correpond au commit A
 je fais une modif 1
 je fais une modif 02 pour le commit 02
+
+>>>>>>> dc9de28723a7b9378b13425eeb3bc61530d2feba
 
